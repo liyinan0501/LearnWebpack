d0019ec102b8f14dc2edf95a37f3085c9a6580cc
Yinan Li <gabriel.li5566@gmail.com>
2022-08-18 00:18:37 +0300

    03-Simple Case with LESS

---
+++ 03-Simple Case with LESS/src/main.js
@@ -0,0 +1,13 @@
+// 1. npm install jquery
+// 2. create public/index.html - 10 li tags
+// 3. main.js
+import $ from 'jquery'
+// 4. using jquery change css
+$('#myUL>li:odd').css('color', 'red')
+$('myUL>li:even').css('color', 'green')
+
+// 5. import css
+import './css/index.css'
+
+// 6. import less
+import './less/index.less'
