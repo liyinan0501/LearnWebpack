1bb440b006347cb6af46c5db1ac5c95ea449c1a1
Yinan Li <gabriel.li5566@gmail.com>
2022-08-17 22:34:28 +0300

    update comments

--- 01-Simple Case/webpack.config.js
+++ 01-Simple Case/webpack.config.js
@@ -7,9 +7,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // 出口路径文件夹名字
     filename: 'bundle.js', // 出口文件名字 (代码打包进这里)
   },
+  // 借助插件，无需单独自行创建 html 文件.
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: './public/index.html', // 参照html文件
     }),
   ],
 }
